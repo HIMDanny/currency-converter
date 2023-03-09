73ea518a551e9241aeeaf71a4dc3ee27e6b3f6d5
HIMDanny <danilbliz0201@gmail.com>
2023-03-09 19:41:22 +0200

    feat: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import convertCurrency from '../api/convertCurrency';
+
+const Header = () => {
+  const [currencies, setCurrencies] = useState({
+    USD: 0,
+    EUR: 0,
+  });
+
+  useEffect(() => {
+    const fetchHeaderCurrencies = async () => {
+      const [usdToUah, eurToUah] = await Promise.all([
+        convertCurrency({ from: 'USD', to: 'UAH', amount: 1 }),
+        convertCurrency({ from: 'EUR', to: 'UAH', amount: 1 }),
+      ]);
+
+      setCurrencies({
+        USD: usdToUah.result,
+        EUR: eurToUah.result,
+      });
+    };
+
+    // fetchHeaderCurrencies();
+  }, []);
+
+  return (
+    <header className="bg-green-700 py-3 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between text-white">
+        <p className="text-xl">Currency converter</p>
+        <div>
+          <span>&#36; {currencies.USD.toFixed(2)}</span> /{' '}
+          <span>&euro; {currencies.EUR.toFixed(2)}</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
