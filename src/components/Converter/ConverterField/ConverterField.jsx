6a72ca4d97dedd8feefcaa861dab572d71fc6379
HIMDanny <danilbliz0201@gmail.com>
2023-03-10 01:19:03 +0200

    feat: create coverter field component

---
+++ src/components/Converter/ConverterField/ConverterField.jsx
@@ -0,0 +1,26 @@
+import ConverterFieldSelect from './ConverterFieldSelect';
+
+const ConverterField = (props) => {
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      <div className="flex mt-1 relative">
+        <input
+          className="bg-slate-50 rounded-l-lg grow p-1 px-2 text-black focus:outline-green-700"
+          type="text"
+          placeholder="100.00"
+          name={props.name}
+          id={props.id}
+          value={props.value}
+          onChange={props.onInputChange}
+        />
+        <ConverterFieldSelect
+          value={props.currencyValue}
+          currencyList={props.currencyList}
+          onChange={props.onCurrencyChange}
+        />
+      </div>
+    </div>
+  );
+};
+export default ConverterField;
