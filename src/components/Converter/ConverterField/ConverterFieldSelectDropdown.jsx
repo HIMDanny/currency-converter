ef8d62ee4254a3a8771e9091d364d4a568c67d1d
HIMDanny <danilbliz0201@gmail.com>
2023-03-10 09:59:22 +0200

    feat: add click outside handler

--- src/components/Converter/ConverterField/ConverterFieldSelectDropdown.jsx
+++ src/components/Converter/ConverterField/ConverterFieldSelectDropdown.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const ConverterFieldSelectDropdown = (props) => {
+  const dropdownRef = useRef(null);
   const [searchTerm, setSearchTerm] = useState('');
 
   const filteredList = props.currencyList.filter((item) =>
@@ -11,7 +12,22 @@ const ConverterFieldSelectDropdown = (props) => {
     setSearchTerm(e.target.value.toUpperCase());
   };
 
-  if (!props.open) return;
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        // possibly not the best solution but that is what I came up with for now
+        setTimeout(() => {
+          props.onClose();
+        }, 0);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
 
   const currencyItems = (
     <ul className="mt-1">
@@ -28,7 +44,10 @@ const ConverterFieldSelectDropdown = (props) => {
   );
 
   return (
-    <div className="absolute right-0 top-full translate-y-2 bg-white text-black z-10 w-40 rounded-lg min-h-[120px] max-h-64 overflow-y-auto shadow-md p-1">
+    <div
+      ref={dropdownRef}
+      className="absolute right-0 top-full translate-y-2 bg-white text-black z-10 w-40 rounded-lg min-h-[120px] max-h-64 overflow-y-auto shadow-md p-1"
+    >
       <input
         type="text"
         placeholder="Search by code"
